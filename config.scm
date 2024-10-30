6b88c815f680496334709579573d8c9e5b7588f2
Akshit Gaur <akshitgaur@proton.me>
2024-10-31 00:32:08 +0530

    initial

---
+++ config.scm
@@ -0,0 +1,74 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu) (nongnu packages linux))
+(use-service-modules cups desktop networking ssh xorg)
+
+(operating-system
+  (kernel linux)
+  (firmware (list linux-firmware))
+  (locale "en_IN.utf8")
+  (timezone "Asia/Kolkata")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "guix")
+
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "akshit")
+                  (comment "Akshit Gaur")
+                  (group "users")
+                  (home-directory "/home/akshit")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages (append (list (specification->package "awesome")
+                          (specification->package "i3-wm")
+                          (specification->package "i3status")
+                          (specification->package "dmenu")
+                          (specification->package "st")) %base-packages))
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (append (list (service gnome-desktop-service-type)
+
+                 ;; To configure OpenSSH, pass an 'openssh-configuration'
+                 ;; record as a second argument to 'service' below.
+                 (service openssh-service-type)
+                 (service tor-service-type)
+                 (set-xorg-configuration
+                  (xorg-configuration (keyboard-layout keyboard-layout))))
+
+           ;; This is the default list of services we
+           ;; are appending to.
+           %desktop-services))
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "b171fd0a-1d5d-4472-a507-e5adf325f7d0"
+                                  'ext4))
+                         (type "ext4"))
+                       (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "6CC4-5E90"
+                                       'fat32))
+                         (type "vfat")) %base-file-systems)))
--- system.scm
+++ system.scm
@@ -1,23 +1,20 @@
 ;; This is an operating system configuration generated
 ;; by the graphical installer.
-;;
-;; Once installation is complete, you can learn and modify
-;; this file to tweak the system configuration, and pass it
-;; to the 'guix system reconfigure' command to effect your
-;; changes.
 
-
-;; Indicate which modules to import to access the variables
-;; used in this configuration.
 (use-modules 
   (gnu) 
+  (gnu services desktop)
   (nongnu packages linux)
   (nongnu system linux-initrd)
+  (nongnu packages nvidia)
+  (nongnu services nvidia)
 )
 (use-service-modules cups desktop networking ssh xorg)
 
 (operating-system
   (kernel linux)
+  (kernel-arguments '("modprobe.blacklist=nouveau"
+                     "nvidia_drm.modeset=1"))
   (initrd microcode-initrd)
   (firmware (list linux-firmware))
   (locale "en_IN.utf8")
@@ -34,38 +31,47 @@
                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
 
-  ;; Packages installed system-wide.  Users can also install packages
-  ;; under their own account: use 'guix search KEYWORD' to search
-  ;; for packages and 'guix install PACKAGE' to install a package.
+  ;; Packages installed system-wide
   (packages (append (list (specification->package "awesome")
-                          (specification->package "i3-wm")
-                          (specification->package "i3status")
-                          (specification->package "dmenu")
-                          (specification->package "st")) %base-packages))
+                         (specification->package "i3-wm")
+                         (specification->package "i3status")
+                         (specification->package "dmenu")
+                         (specification->package "st"))
+                   %base-packages))
 
-  ;; Below is the list of system services.  To search for available
-  ;; services, run 'guix system search KEYWORD' in a terminal.
-  (services
-   (append (list (service gnome-desktop-service-type)
+  ;; Services configuration
+  (services (append
+    (list 
+      (service nvidia-service-type)
+      ;; Configure GDM login manager
+      (service gnome-desktop-service-type)
+      ;; Configure Xorg with NVIDIA support
+      (set-xorg-configuration
+       (xorg-configuration
+        (keyboard-layout keyboard-layout)
+        (modules (cons nvda %default-xorg-modules))
+        (drivers '("nvidia"))))
+      ;; Added OpenSSH service with updated configuration
+      ;;(service openssh-service-type
+      ;;         (openssh-configuration
+      ;;          (permit-root-login 'prohibit-password)
+      ;;          (password-authentication? #f)))
+      ;; Added basic Tor service
+      ;; (service tor-service-type)
+      )
+    %desktop-services
+    ;; Use desktop services but remove conflicting ones
+    ;;(modify-services %desktop-services
+    ;;                (delete gdm-service-type)
+    ;;                )
+    ))
 
-                 ;; To configure OpenSSH, pass an 'openssh-configuration'
-                 ;; record as a second argument to 'service' below.
-                 (service openssh-service-type)
-                 (service tor-service-type)
-                 (set-xorg-configuration
-                  (xorg-configuration (keyboard-layout keyboard-layout))))
-
-           ;; This is the default list of services we
-           ;; are appending to.
-           %desktop-services))
   (bootloader (bootloader-configuration
                 (bootloader grub-efi-bootloader)
                 (targets (list "/boot/efi"))
                 (keyboard-layout keyboard-layout)))
 
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
+  ;; File systems configuration
   (file-systems (cons* (file-system
                          (mount-point "/")
                          (device (uuid
@@ -77,9 +83,10 @@
                          (device (uuid "6CC4-5E90"
                                        'fat32))
                          (type "vfat"))
-  		       (file-system
-			 (mount-point "/home/akshit/storage")
-			 (device (uuid
-				   "44d8d8ec-d939-409c-93c2-22c955f5111c"
-				   'ext4))
-			 (type "ext4")) %base-file-systems)))
+                       (file-system
+                         (mount-point "/home/akshit/storage")
+                         (device (uuid
+                                  "44d8d8ec-d939-409c-93c2-22c955f5111c"
+                                  'ext4))
+                         (type "ext4"))
+                       %base-file-systems)))
